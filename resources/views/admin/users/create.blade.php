f9f2aa8727abd8380523912b429a8439be6ac441
Felipe Schmitz <schmitzdz14@gmail.com>
2025-03-03 09:58:39 -0300

    feat: implement user creation with validation

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,13 +3,21 @@
 @section('title', 'Criar novo Usuário')
 @section('content')
 
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
     <form action="{{ route('users.store') }}" method="POST">
         {{-- Token --}}
         @csrf()
         {{--  --}}
 
-        <input type="text" name="name" id="name" placeholder="Nome">
-        <input type="email" name="email" id="email" placeholder="Email">
+        <input type="text" name="name" id="name" placeholder="Nome" value="{{ old('name') }}">
+        <input type="email" name="email" id="email" placeholder="Email" value="{{ old('email') }}">
         <input type="passsword" name="password" id="password" placeholder="Senha">
         <input type="submit" value="Enviar">
     </form>
